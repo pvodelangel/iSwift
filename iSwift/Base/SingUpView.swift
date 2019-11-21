c4b1bb390557209d1d5978464110468e52b122b7
Vanessa Orozco Del Angel <>
2019-11-21 17:15:45 -0600

    NEW: Loggin

---
+++ iSwift/Base/SingUpView.swift
@@ -0,0 +1,24 @@
+//
+//  SingUpView.swift
+//  iSwift
+//
+//  Created by Paola Vanessa Orozco Del Angel on 21/11/19.
+//  Copyright © 2019 Manuel Alejandro Aguilar Tellez Giron. All rights reserved.
+//
+
+import UIKit
+
+class SingUpView: UIViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+   
+
+}
