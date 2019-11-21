ba298ccdf9e75e118dc861ed0c6d7735670d315b
FixingMind5 <aguilarmanu5@gmail.com>
2019-11-21 13:40:05 -0600

    refactorizing

---
+++ iSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iSwift
+//
+//  Created by Manuel Alejandro Aguilar Tellez Giron on 21/11/19.
+//  Copyright © 2019 Manuel Alejandro Aguilar Tellez Giron. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
