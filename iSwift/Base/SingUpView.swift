f835c6c9c0f7aa1056ec2c19e3839b5e65087ce1
Vanessa Orozco Del Angel <>
2019-11-21 18:56:46 -0600

    NEW: Diseño de Inicio de Sesión y Registro

--- iSwift/Base/SingUpView.swift
+++ iSwift/Base/SingUpView.swift
@@ -8,17 +8,52 @@
 
 import UIKit
 
-class SingUpView: UIViewController {
+class SingUpView: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
+    @IBOutlet weak var botonStyleRegistro: UIButton!
+    @IBOutlet weak var nombre: UITextField!
+    @IBOutlet weak var apellidoPaterno: UITextField!
+    @IBOutlet weak var apellidoMaterno: UITextField!
+    @IBOutlet weak var correoElectronicoR: UITextField!
+    @IBOutlet weak var numeroCuenta: UITextField!
+    @IBOutlet weak var contrasenaR: UITextField!
+    @IBOutlet weak var pickerOutlet: UIPickerView!
     
+    var pickerData: [String] = [String]()
+    var carrera:String = String()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+    botonStyleRegistro.layer.cornerRadius = 10
+        self.pickerOutlet.delegate = self as! UIPickerViewDelegate
+        self.pickerOutlet.dataSource = self as! UIPickerViewDataSource
+        
+    pickerData = ["Ingeniería Civil", "Matemáticas Aplicadas y Computación", "Actuaría", "Diseño Gráfico", "Arquitectura", "Derecho", "Ciencias Políticas y Administración Pública", "Economía","Relaciones Internacionales", "Sociología", "Comunicación", "Enseñanza de Inglés", "Filosofía", "Historia",
+    "Lengua y Literatura Hispánicas",
+    "Pedagogía"]
         
     }
     
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        carrera = pickerData[row]
+    }
 
-   
+    @IBAction func BotonRegistro(_ sender: Any) {
+        
+    }
+    
 
 }
